c7905c4fac75170ba3fad0f4675a6544babae1b7
A1-mamun <1154almamun@gmail.com>
2024-05-10 23:30:06 +0600

    firebase Auth Provider created

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,91 @@
+import PropTypes from "prop-types";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+
+const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+// social log in providers
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const AuthProvider = ({ children }) => {
+  const [user, setuser] = useState();
+  const [loading, setLoading] = useState();
+
+  // sign up
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   User profile Update
+  const profileUpdate = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  //   signIn user
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //   sign in google
+  const signInGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //   sign in github
+  const signInGithub = () => {
+    setLoading(true);
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  //   sign out user
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  //   user state managemnet
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setuser(currentUser);
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    profileUpdate,
+    signIn,
+    signInGoogle,
+    signInGithub,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
